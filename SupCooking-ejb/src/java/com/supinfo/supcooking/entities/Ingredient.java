e8e78c944541d8516b5e77dea8fc3cc89abde9b4
Quentin Lamotte <quentin.francois.lamotte@gmail.com>
2018-01-28 16:07:47 +0100

    change: precised primary keys in entities

--- SupCooking-ejb/src/java/com/supinfo/supcooking/entities/Ingredient.java
+++ SupCooking-ejb/src/java/com/supinfo/supcooking/entities/Ingredient.java
@@ -6,7 +6,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -16,7 +15,7 @@ public class Ingredient implements Serializable {
     
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     @NotEmpty
--- SupCooking-ejb/src/java/com/supinfo/supcooking/entities/Recipe.java
+++ SupCooking-ejb/src/java/com/supinfo/supcooking/entities/Recipe.java
@@ -23,7 +23,7 @@ public class Recipe implements Serializable {
     private static final long serialVersionUID = 1L;
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     
